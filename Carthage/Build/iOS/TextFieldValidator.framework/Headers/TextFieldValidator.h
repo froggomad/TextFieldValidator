2afb96dad72afc9e45b7d1d2b19bce9d4e88561a
Kenny Dubroff <kenny.dubroff@kennydubroff.com>
2020-05-16 05:25:00 -0700

    removed .gitignore

---
+++ Carthage/Build/iOS/TextFieldValidator.framework/Headers/TextFieldValidator.h
@@ -0,0 +1,19 @@
+//
+//  EmptyTextfieldAlerter.h
+//  EmptyTextfieldAlerter
+//
+//  Created by Kenny on 5/15/20.
+//  Copyright © 2020 Hazy Studios. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EmptyTextfieldAlerter.
+FOUNDATION_EXPORT double EmptyTextfieldAlerterVersionNumber;
+
+//! Project version string for EmptyTextfieldAlerter.
+FOUNDATION_EXPORT const unsigned char EmptyTextfieldAlerterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EmptyTextfieldAlerter/PublicHeader.h>
+
+
